7b5708ffdd9bf0fb47c5acb95d8c7a4bbceb6398
MITSUNARI Shigeo <herumi@nifty.com>
2016-05-17 09:21:35 +0900

    use /MT option

--- mk.bat
+++ mk.bat
@@ -1,2 +1,2 @@
-set INC=-I ../cybozulib/include  -I ../xbyak/ -I../mcl/include -I ../cybozulib_ext/mpir/include
-cl /Ox /EHsc /MD /DNOMINMAX %INC% add_he.cpp ../mcl/src/fp.cpp -DMCL_NO_AUTOLINK /link /LIBPATH:../cybozulib_ext/mpir/lib
+set INC=-I ../cybozulib/include  -I ../xbyak/ -I../mcl/include -I ../cybozulib_ext/include
+cl /Ox /EHsc /MT /DNOMINMAX %INC% add_he.cpp ../mcl/src/fp.cpp -DMCL_NO_AUTOLINK /link /LIBPATH:../cybozulib_ext/lib
